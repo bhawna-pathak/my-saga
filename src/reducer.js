223a2ea779bd24467843c49846ef80dd1679cf7f
bhawna-pathak <bhawna9123@gmail.com>
2021-12-07 09:12:09 +0530

    creation of user list

--- src/reducer.js
+++ src/reducer.js
@@ -8,7 +8,6 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-  console.log(action.type);
   switch (action.type) {
     case `${USER_FETCH}_PENDING`: {
       return {
